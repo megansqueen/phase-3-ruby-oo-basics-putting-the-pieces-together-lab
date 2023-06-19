c1bd59b0d200ceb75d556a20d606ff3309e8290d
Megan <esler6724@gmail.com>
2023-06-19 13:09:09 -0700

    commit tree 969dc3577765c3af2dbe7690ca455174330eb566

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    # def initialize(author)
+    #     @author = author
+    # end
+    # def initialize(page_count)
+    #     @page_count = page_count
+    # end
+end
 
 
